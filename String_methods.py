b5425af86f87feaf25befa2726a1442737eb0ca0
nalin <nalinikantbastia@gmail.com>
2026-01-01 19:34:22 +0530

    string methods part 1

---
+++ String_methods.py
@@ -0,0 +1,31 @@
+text = "PythOn"
+
+# to lowercase
+lowertxt =text.lower()
+print(lowertxt)
+
+# uppercase
+uppertxt = text.upper()
+print(uppertxt)
+
+# capitalize
+capitaltxt = text.capitalize()
+print(capitaltxt)
+
+# swapcase(converts uppercase to lower , and lowercase to upper)
+swaptxt = text.swapcase()
+print(swaptxt)
+
+#removing spaces
+#  strip
+msg = "   Hello World   "
+stripmsg = msg.strip()
+print(stripmsg)
+
+# lstrip
+lstripmsg = msg.lstrip()
+print(lstripmsg)
+
+# rstrip
+rstripmsg = msg.rstrip()
+print(rstripmsg)
