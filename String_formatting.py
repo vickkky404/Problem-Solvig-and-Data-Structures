41f394dca39496db3fa9698255df614e7fa5fb8a
nalin <nalinikantbastia@gmail.com>
2026-01-01 19:13:20 +0530

    formatting of string in python

---
+++ String_formatting.py
@@ -0,0 +1,4 @@
+a = 21
+ #txt = "Iam Nalinikant, my age is " + a #this will generate an error  "TypeError: can only concatenate str (not "int") to str"
+txt = f"My Name is Nalinikant, my age is {a}"
+print(txt)
