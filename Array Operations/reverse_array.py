bec3d50d255b4c85bd3b50ca00838362955c7ed0
nalin <nalinikantbastia@gmail.com>
2026-01-05 20:21:37 +0530

    reverse an array using python

---
+++ Array Operations/reverse_array.py
@@ -0,0 +1,14 @@
+import array as arr
+
+val = arr.array('i' , [1,2,3,4,5,6])
+print(val)
+
+print("\n")
+
+print("Reversed Array")
+
+val.reverse()
+for i in range(len(val)):
+    print(val[i], end=" ")
+
+
