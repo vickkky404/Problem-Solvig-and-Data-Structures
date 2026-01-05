bcc1106c58519f439f57b645e1ed58c9aa73f50b
nalin <nalinikantbastia@gmail.com>
2026-01-05 20:14:05 +0530

    array declaration and printing in using array module

---
+++ Array Operations/OperationInArray.py
@@ -0,0 +1,17 @@
+import array as arr
+
+val = arr.array('i' , [1,2,3,4,5,6,7])
+print(val)
+
+# printing using basic for loop and advanced for loop
+
+# basic for loop
+for i in range(len(val)):
+    print(val[i])
+
+# advanced for loop
+print('\n')
+print("Advanced for loop output")
+
+for x in val:
+    print(x)
