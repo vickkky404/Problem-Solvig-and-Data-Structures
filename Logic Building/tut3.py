5a0c56a0285332dd4afa51f0917786b083fd3fb8
nalin <nalinikantbastia@gmail.com>
2026-01-08 21:52:03 +0530

    logic building 3

---
+++ Logic Building/tut3.py
@@ -0,0 +1,13 @@
+
+counter = 1
+
+while counter <= 10:
+    print(counter)
+
+    # Increment the counter using an if statement
+    if counter < 10:
+        counter = counter + 1
+    else:
+
+        break
+
