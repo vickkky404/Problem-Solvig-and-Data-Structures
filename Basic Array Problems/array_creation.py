a0670d594ab48beb61578ea6d484388a4aca6f23
nalin <nalinikantbastia@gmail.com>
2026-01-24 20:40:48 +0530

    array basics using list

---
+++ Basic Array Problems/array_creation.py
@@ -0,0 +1,14 @@
+#creating a basic array using list
+
+cars = ["Ford", "Maruti", "Tata"]
+print(cars)
+
+# printing perticular index
+print(cars[1])
+
+# finding the length of the array
+print(len(cars))
+
+# printing the elements in the array using loop
+for car in cars:
+    print(car)
