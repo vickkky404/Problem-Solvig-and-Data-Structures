33d4c0c8d3ca0b9fa655c678373888116f42d747
nalin <nalinikantbastia@gmail.com>
2026-01-24 20:57:19 +0530

    Find the Sum of All Elements in an Array

---
+++ Basic Array Problems/sumof_allElements.py
@@ -0,0 +1,11 @@
+#Find the Sum of All Elements in an Array
+# nput:
+#2 4 6 8 10
+
+numbers = [2, 4, 6, 8, 10]
+total_sum = 0
+
+for num in numbers:
+    total_sum = total_sum + num
+
+print("The sum of all elements in the array is:", total_sum)
